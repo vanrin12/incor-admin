e3974d31c2b74fac6df48d3092bde4f8a1d17ef7
Tình Nguyễn <nguyenminhtinh1209@gmail.com>
2020-12-14 21:50:29 +0700

    register post ui

--- src/mockData/dataSelect.js
+++ src/mockData/dataSelect.js
@@ -96,4 +96,44 @@ const timeProject = [
   },
 ];
 
-export { headquarters, job, vote, role, timeProject };
+const listSpace = [
+  {
+    id: 0,
+    value: 'Không gian kinh doanh',
+    label: 'Không gian kinh doanh',
+  },
+  {
+    id: 1,
+    value: 'Không gian nhà ở',
+    label: 'Không gian nhà ở',
+  },
+  {
+    id: 2,
+    name: 'Không gian làm việc',
+    label: 'Không gian làm việc',
+  },
+];
+const listTypeSpace = [
+  {
+    id: 0,
+    value: 'chung cư',
+    label: 'chung cư',
+  },
+  {
+    id: 1,
+    value: 'nhà phố',
+    label: 'nhà phố',
+  },
+  {
+    id: 2,
+    name: 'biệt thự',
+    label: 'biệt thự',
+  },
+  {
+    id: 3,
+    name: 'nhà cấp 4',
+    label: 'nhà cấp 4',
+  },
+];
+
+export { headquarters, job, vote, role, timeProject, listSpace, listTypeSpace };
