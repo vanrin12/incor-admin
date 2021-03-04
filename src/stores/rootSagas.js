284468e6f0dd5ef46b7526b8822527ab165e140d
Rin To <rin.to@ enjoyworks.co.kr>
2021-03-04 08:17:51 +0700

    Update api link

--- src/stores/rootSagas.js
+++ src/stores/rootSagas.js
@@ -3,7 +3,13 @@ import { all } from 'redux-saga/effects';
 import getDataMainSaga from 'modules/home/sagas/getDataMainSaga';
 import getListUserSaga from 'modules/accounts/sagas/getListUserSaga';
 import getListRolesSaga from 'modules/accounts/sagas/getListRolesSaga';
+import createUserSaga from 'modules//accounts/sagas/createUserSaga';
 
 export default function* RootSagas() {
-  yield all([getDataMainSaga(), getListUserSaga(), getListRolesSaga()]);
+  yield all([
+    getDataMainSaga(),
+    getListUserSaga(),
+    getListRolesSaga(),
+    createUserSaga(),
+  ]);
 }
